66e61a3b733c7741d9241ce7de378857bd071971
Robin Uth <robin.uth95@gmail.com>
2018-05-23 15:04:19 +0200

    Initial commit

---
+++ Source/FPSProjekt/FPSProjektGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSProjektGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSPROJEKT_API AFPSProjektGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
